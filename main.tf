270fec2479c951dc5ba82eedabe83df1b04d0ad9
Mike Peachey <mike.peachey@bjss.com>
2016-08-11 19:01:27 +0100

    refactoring

--- main.tf
+++ main.tf
@@ -19,32 +19,18 @@ module "microservice_pritest" {
   }
 }
 
-resource "aws_route_table" "private_nats" {
-  # https://github.com/hashicorp/terraform/issues/1497
-  #count = "${length(data.aws_availability_zones.available.names)}"
-  count = 3
-  vpc_id = "${var.vpc_id}"
-  tags {
-    Environment = "${var.environment}"
-    Application = "${var.project}"
-    Tier        = "PRI"
-  } 
-}
+module "ha-nat" {
+  source                          = "../terraform-ha-nat"
+  availability_zones              = "${data.aws_availability_zones.available.names}"
+  aws_region                      = "${var.aws_region}"
+  name                            = "ha-nat"
+  subnets_cidr                    = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  subnets_route_tables            = ["${aws_route_table.public.id}"]
+  vpc_id                          = "${var.vpc_id}"
 
-resource "aws_route_table" "private_nonat" {
-  vpc_id = "${var.vpc_id}"
-  tags {
-    Environment = "${var.environment}"
-    Application = "${var.project}"
-    Tier        = "PRI"
-  } 
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = "${var.vpc_id}"
   tags {
     Environment = "${var.environment}"
     Application = "${var.project}"
     Tier        = "PUB"
-  } 
+  }
 }
---
+++ route_tables.tf
@@ -0,0 +1,29 @@
+resource "aws_route_table" "private_nats" {
+  # https://github.com/hashicorp/terraform/issues/1497
+  #count = "${length(data.aws_availability_zones.available.names)}"
+  count = 3
+  vpc_id = "${var.vpc_id}"
+  tags {
+    Environment = "${var.environment}"
+    Application = "${var.project}"
+    Tier        = "PRI"
+  }
+}
+
+resource "aws_route_table" "private_nonat" {
+  vpc_id = "${var.vpc_id}"
+  tags {
+    Environment = "${var.environment}"
+    Application = "${var.project}"
+    Tier        = "PRI"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${var.vpc_id}"
+  tags {
+    Environment = "${var.environment}"
+    Application = "${var.project}"
+    Tier        = "PUB"
+  }
+}
