2455f09e1fb86fc6b38d363a9bea2ef18d008105
Mike Peachey <mike.peachey@bjss.com>
2016-08-18 18:30:14 +0100

    cleanup outputs a bit

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,15 @@
-output "core_sg_id" {
-  value = "${aws_security_group.core.id}"
-}
-
-output "azs_available_names" {
+output "availability_zones" {
   value = "${data.aws_availability_zones.available.names}"
-}
-
-output "private_nats_ids" {
-  value = ["${aws_route_table.private_nats.*.id}"]
 }
 
 output "aws_region" {
   value = "${var.aws_region}"
 }
 
-output "region_az_count" {
-  value = "${var.region_az_count}"
+output "core_sg_id" {
+  value = "${aws_security_group.core.id}"
 }
 
-output "rtb_public_id" {
-  value = "${aws_route_table.public.id}"
+output "region_az_count" {
+  value = "${var.region_az_count}"
 }
