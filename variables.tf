61175eb9c3747d139a20b01d9b46c07e7e4dba83
Sebastian Mincewicz <s.mincewicz@kainos.com>
2016-08-23 18:22:29 +0100

    change things to make tests working

--- variables.tf
+++ variables.tf
@@ -28,24 +28,30 @@ variable "region_az_count" {
   description = "This is a dirty horrible hack to circumvent the issue raised in https://github.com/hashicorp/terraform/issues/1497. counts are graphed too early to allow interpolation and until Hashicorp implement JIT graphing for count, there are few ways to define a count for derived lists"
 }
 
+variable "bucket_name" {
+  type        = "string"
+  description = "Bucket name"
+  default     = ""
+}
+
 ### The following are here temporarily until invoked via remote state access to the bootstrap module ###
 
-variable "internet_gateway_id" {
-  type        = "string"
-  description = "$${remote_state.aws_internet_gateway.internet_gateway.id}"
-}
+# variable "internet_gateway_id" {
+#   type        = "string"
+#   description = "$${remote_state.aws_internet_gateway.internet_gateway.id}"
+# }
 
-variable "private_nat_route_table_ids" {
-  type        = "string"
-  description = "[$${remote_state.aws_route_table.private_nats.*.id}]"
-}
+# variable "private_nat_route_table_ids" {
+#   type        = "string"
+#   description = "[$${remote_state.aws_route_table.private_nats.*.id}]"
+# }
 
-variable "private_nonat_route_table_id" {
-  type        = "string"
-  description = "$${remote_state.aws_route_table.private-nonat.id}"
-}
+# variable "private_nonat_route_table_id" {
+#   type        = "string"
+#   description = "$${remote_state.aws_route_table.private-nonat.id}"
+# }
 
-variable "public_route_table_id" {
-  type        = "string"
-  description = "$${remote_state.aws_route_table.public.id}"
-}
+# variable "public_route_table_id" {
+#   type        = "string"
+#   description = "$${remote_state.aws_route_table.public.id}"
+# }
